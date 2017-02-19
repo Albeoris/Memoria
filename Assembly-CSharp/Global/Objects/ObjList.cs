853a811e2347bb58d959cc0296ca35ec12cdc90e
Albeoris <albeoris@gmail.com>
2017-02-19 17:42:08 +0300

    Lost files

---
+++ Assembly-CSharp/Global/Objects/ObjList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class ObjList
+{
+	public void copy(ObjList ol)
+	{
+		if (ol.obj == null)
+		{
+			return;
+		}
+		if (ol.obj.cid != 0)
+		{
+			if (ol.obj.cid == 1)
+			{
+				this.obj = new Seq();
+			}
+			else if (ol.obj.cid == 2)
+			{
+				this.obj = new Obj();
+			}
+			else if (ol.obj.cid == 3)
+			{
+				this.obj = new Quad();
+			}
+			else if (ol.obj.cid == 4)
+			{
+				this.obj = new Actor();
+			}
+		}
+		if (ol.obj != null)
+		{
+			this.obj.copy(ol.obj);
+		}
+		if (this.next != null)
+		{
+			this.next = (ObjList)null;
+		}
+	}
+
+	public ObjList next;
+
+	public Obj obj;
+}
---
+++ Assembly-CSharp/Global/Objects/ObjTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+public class ObjTable
+{
+	public void ReadData(BinaryReader br)
+	{
+		this.ofs = br.ReadInt16();
+		this.size = br.ReadInt16();
+		this.varn = br.ReadByte();
+		this.flags = br.ReadByte();
+		this.pad1 = br.ReadByte();
+		this.pad2 = br.ReadByte();
+	}
+
+	public Int16 ofs;
+
+	public Int16 size;
+
+	public Byte varn;
+
+	public Byte flags;
+
+	public Byte pad1;
+
+	public Byte pad2;
+}
