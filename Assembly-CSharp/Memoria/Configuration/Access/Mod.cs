12e15412dff5b18e892655836115b949333b9e51
snouz <snooz-x@hotmail.com>
2020-06-28 20:31:25 +0200

    sync (#9)
    * Reworked many parts
    
    * Add missing file 1/2
    
    * Add missing file 2/2
    
    Co-authored-by: Albeoris <albeoris@gmail.com>
    Co-authored-by: Tirlititi <laroche.clement1@gmail.com>

---
+++ Assembly-CSharp/Memoria/Configuration/Access/Mod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Memoria
+{
+    public sealed partial class Configuration
+    {
+        public static class Mod
+        {
+            public static String[] FolderNames => Instance._mod.FolderNames;
+        }
+    }
+}
---
+++ Assembly-CSharp/Memoria/Configuration/Structure/ModSection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Memoria.Prime.Ini;
+
+namespace Memoria
+{
+    public sealed partial class Configuration
+    {
+        private sealed class ModSection : IniSection
+        {
+            public readonly IniArray<String> FolderNames;
+
+            public ModSection() : base(nameof(ModSection), true)
+            {
+                FolderNames = BindStringArray(nameof(FolderNames), new String[0]);
+            }
+        }
+    }
+}
