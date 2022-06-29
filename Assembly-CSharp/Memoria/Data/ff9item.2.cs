284ec1a89122a130f6537e0cd73a4296a5e1d2c2
Piotrowski, Maciej <maciej.piotrowski@accenture.com>
2022-06-29 16:51:37 +0200

    Sorted using and create pure function for GetPtr

--- Assembly-CSharp/Memoria/Data/ff9item.2.cs
+++ Assembly-CSharp/Memoria/Data/ff9item.2.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.IO;
-using Assets.SiliconSocial;
+﻿using Assets.SiliconSocial;
 using FF9;
 using Memoria;
 using Memoria.Assets;
 using Memoria.Data;
 using Memoria.Prime;
 using Memoria.Prime.CSV;
+using System;
+using System.IO;
 
 // ReSharper disable FieldCanBeMadeReadOnly.Global
 // ReSharper disable UnusedMember.Global
@@ -159,6 +159,18 @@ public class ff9item
     public static FF9ITEM FF9Item_GetPtr(Int32 id)
     {
         FF9ITEM[] ff9ItemArray = FF9StateSystem.Common.FF9.item;
+        for (Int32 index = 0; index < 256; ++index)
+        {
+            FF9ITEM ff9Item = ff9ItemArray[index];
+            if (ff9Item.count != 0 && ff9Item.id == id)
+                return ff9Item;
+        }
+        return null;
+    }
+
+    public static FF9ITEM FF9Item_GetPtr(Int32 id, FF9ITEM[] ff9Items)
+    {
+        FF9ITEM[] ff9ItemArray = ff9Items;
         for (Int32 index = 0; index < 256; ++index)
         {
             FF9ITEM ff9Item = ff9ItemArray[index];
