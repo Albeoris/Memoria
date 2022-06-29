172d4b97a9b780d8d820e9064a653a01e649a247
Piotrowski, Maciej <maciej.piotrowski@accenture.com>
2022-06-29 16:59:41 +0200

    Moved function outside of ff9item class so it can be tested

---
+++ Assembly-CSharp/Memoria/Data/ff9itemHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class ff9itemHelpers
+{
+
+    public static FF9ITEM FF9Item_GetPtr(Int32 id, FF9ITEM[] ff9Items)
+    {
+        FF9ITEM[] ff9ItemArray = ff9Items;
+        for (Int32 index = 0; index < 256; ++index)
+        {
+            FF9ITEM ff9Item = ff9ItemArray[index];
+            if (ff9Item.count != 0 && ff9Item.id == id)
+                return ff9Item;
+        }
+        return null;
+    }
+}
