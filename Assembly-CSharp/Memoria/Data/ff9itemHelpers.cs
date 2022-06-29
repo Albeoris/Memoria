7ee818781c7784a61ac1d5e81169ef168a3e0e87
Piotrowski, Maciej <maciej.piotrowski@accenture.com>
2022-06-29 17:09:26 +0200

    Refac. GetPtr method

--- Assembly-CSharp/Memoria/Data/ff9itemHelpers.cs
+++ Assembly-CSharp/Memoria/Data/ff9itemHelpers.cs
@@ -1,17 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 public static class ff9itemHelpers
 {
 
     public static FF9ITEM FF9Item_GetPtr(Int32 id, FF9ITEM[] ff9Items)
     {
-        FF9ITEM[] ff9ItemArray = ff9Items;
-        for (Int32 index = 0; index < 256; ++index)
-        {
-            FF9ITEM ff9Item = ff9ItemArray[index];
-            if (ff9Item?.count != 0 && ff9Item?.id == id)
-                return ff9Item;
-        }
-        return null;
+        return ff9Items.FirstOrDefault(x => x?.count != 0 && x?.id == id);
     }
 }
