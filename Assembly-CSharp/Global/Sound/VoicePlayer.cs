6865f5e3867ce4ea4b3c0b5b8f160b4bdb1a0bd4
Martin Barker <martin-barker@hotmail.co.uk>
2022-10-24 00:17:42 +0100

    Added support from blocking the same line multiple times when a group chat happens E.G VA_46 at the start of the game

--- Assembly-CSharp/Global/Sound/VoicePlayer.cs
+++ Assembly-CSharp/Global/Sound/VoicePlayer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Memoria;
 using Memoria.Assets;
 using UnityEngine;
@@ -24,6 +25,44 @@ public class VoicePlayer : SoundPlayer
 		this.stateTransition.Add(new SdLibSoundProfileStateGraph.TransitionDelegate(base.StopSound), SoundProfileState.Paused, SoundProfileState.Stopped);
 		this.stateTransition.Add(new SdLibSoundProfileStateGraph.TransitionDelegate(base.StopSound), SoundProfileState.Played, SoundProfileState.Stopped);
 		this.stateTransition.Add(new SdLibSoundProfileStateGraph.TransitionDelegate(base.StopSound), SoundProfileState.CrossfadeIn, SoundProfileState.Stopped);
+	}
+
+	/*
+	 * Overrides to stop duplicate files playing when multiple text boxes appear at once
+	 */
+
+	private static Dictionary<string, Boolean> preventMultiPlay = new Dictionary<string, Boolean>()
+	{
+		["Voices/US/2/va_46"] = false
+	};
+
+	new public void StartSound(SoundProfile soundProfile, Single playerVolume = 1f) => StaticStartSound(soundProfile, playerVolume);
+
+	new public static void StaticStartSound(SoundProfile soundProfile, Single playerVolume = 1f)
+	{
+        if (preventMultiPlay.ContainsKey(soundProfile.Name) && preventMultiPlay[soundProfile.Name]) {
+			return;
+        }
+        else
+        {
+			if (preventMultiPlay.ContainsKey(soundProfile.Name))
+			{
+				preventMultiPlay[soundProfile.Name] = true;
+			}
+		}
+
+		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_Start(soundProfile.SoundID, 0);
+		if (ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_IsExist(soundProfile.SoundID) == 0)
+		{
+			SoundLib.Log("failed to play sound");
+			soundProfile.SoundID = 0;
+			return;
+		}
+		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetVolume(soundProfile.SoundID, soundProfile.SoundVolume * playerVolume, 0);
+		SoundLib.Log("Panning: " + soundProfile.Panning);
+		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetPanning(soundProfile.SoundID, soundProfile.Panning, 0);
+		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetPitch(soundProfile.SoundID, soundProfile.Pitch, 0);
+		SoundLib.Log("StartSound Success");
 	}
 
 	public static void PlayBattleVoice(Int32 va_id, String text, Boolean asSharedMessage = false, Int32 btlId = -1)
@@ -67,148 +106,6 @@ public class VoicePlayer : SoundPlayer
 		ETb.voiceDatabase);
 	}
 
-	public void LoadMusic(String metaData)
-	{
-		base.LoadResource(metaData, this.soundDatabase, new SoundPlayer.LoadResourceCallback(this.LoadSoundResourceCallback));
-	}
-
-	private void LoadSoundResourceCallback(SoundDatabase soundDatabase, Boolean isError)
-	{
-		SoundLib.Log("LoadVoiceResourceCallback: " + ((!isError) ? "Success" : "Error"));
-	}
-
-	public void UnloadMusic()
-	{
-		base.UnloadResource(this.soundDatabase);
-	}
-
-	public void PlayMusic(Int32 soundIndex)
-	{
-		this.PlayMusic(soundIndex, 0, SoundProfileType.Music);
-	}
-
-	public void PlayMusic(Int32 soundIndex, Int32 fadeIn, SoundProfileType type = SoundProfileType.Music)
-	{
-		SoundProfile soundProfile = this.soundDatabase.Read(soundIndex);
-		if (soundProfile == null)
-		{
-			soundProfile = this.onTheFlySoundDatabase.Read(soundIndex);
-		}
-		if (soundProfile != null)
-		{
-			this.PlayMusic(soundProfile, fadeIn);
-		}
-		else
-		{
-			soundProfile = SoundMetaData.GetSoundProfile(soundIndex, type);
-			this.onTheFlyLoadedSoundProfile = soundProfile;
-			this.onTheFlyLoadedFadeIn = fadeIn;
-			if (this.onTheFlySoundDatabase.ReadAll().Count >= 10)
-			{
-				SoundLib.Log("Unload on the fly voice database.");
-				base.UnloadResource(this.onTheFlySoundDatabase);
-			}
-			base.LoadResource(soundProfile, this.onTheFlySoundDatabase, new SoundPlayer.LoadResourceCallback(this.LoadOnTheFlySoundResourceCallback));
-		}
-	}
-
-	public void NextLoopRegion(Int32 soundIndex)
-	{
-		SoundProfile soundProfile = this.soundDatabase.Read(soundIndex);
-		if (soundProfile == null)
-		{
-			soundProfile = this.onTheFlySoundDatabase.Read(soundIndex);
-		}
-		if (soundProfile != null)
-		{
-			ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetNextLoopRegion(soundProfile.SoundID);
-		}
-		else
-		{
-			SoundLib.Log("NextLoopRegion(), soundProfile is null!");
-		}
-	}
-
-	private void LoadOnTheFlySoundResourceCallback(SoundDatabase soundDatabase, Boolean isError)
-	{
-		if (!isError)
-		{
-			if (this.onTheFlyLoadedSoundProfile != null)
-			{
-				this.PlayMusic(this.onTheFlyLoadedSoundProfile, this.onTheFlyLoadedFadeIn);
-			}
-		}
-		else
-		{
-			SoundLib.Log("LoadOnTheFlySoundResourceCallback is Error");
-		}
-	}
-
-	public void PlayMusic(SoundProfile soundProfileFromIndex, Int32 fadeIn)
-	{
-		this.fadeInDuration = (Single)fadeIn / 1000f;
-		this.fadeInTimeRemain = this.fadeInDuration;
-		if (this.activeSoundProfile == soundProfileFromIndex)
-		{
-			if (this.activeSoundProfile.SoundProfileState == SoundProfileState.Paused)
-			{
-				this.stateTransition.Transition(soundProfileFromIndex, new SdLibSoundProfileStateGraph.TransitionDelegate(base.ResumeSound));
-				this.SetMusicVolume(this.playerVolume * this.optionVolume, soundProfileFromIndex);
-				this.SetMusicPanning(this.playerPanning, soundProfileFromIndex);
-				this.SetMusicPitch(this.playerPitch, soundProfileFromIndex);
-			}
-		}
-		else if (this.upcomingSoundProfile == soundProfileFromIndex)
-		{
-			if (this.activeSoundProfile != null && this.stateTransition.Transition(this.activeSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.StopSound)) == 0)
-			{
-				this.activeSoundProfile = (SoundProfile)null;
-			}
-			if (this.upcomingSoundProfile != null && (this.upcomingSoundProfile.SoundProfileState == SoundProfileState.CrossfadeIn || this.upcomingSoundProfile.SoundProfileState == SoundProfileState.Paused))
-			{
-				this.stateTransition.Transition(this.upcomingSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.ResumeSound));
-				this.upcomingSoundProfile.SoundVolume = this.playerVolume * this.optionVolume;
-				ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetVolume(this.upcomingSoundProfile.SoundID, this.playerVolume * this.optionVolume, (Int32)(this.fadeInTimeRemain * 1000f));
-				this.SetMusicPanning(this.playerPanning, this.upcomingSoundProfile);
-				this.SetMusicPitch(this.playerPitch, this.upcomingSoundProfile);
-				this.activeSoundProfile = this.upcomingSoundProfile;
-				this.activeSoundProfile.SoundProfileState = SoundProfileState.Played;
-				this.upcomingSoundProfile = (SoundProfile)null;
-			}
-		}
-		else
-		{
-			if (this.upcomingSoundProfile != null)
-			{
-				if (this.activeSoundProfile != null)
-				{
-					if (this.stateTransition.Transition(this.activeSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.StopSound)) == 0)
-					{
-						this.activeSoundProfile = this.upcomingSoundProfile;
-						this.upcomingSoundProfile = (SoundProfile)null;
-					}
-				}
-				else
-				{
-					ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_Stop(this.upcomingSoundProfile.SoundID, fadeIn);
-					this.upcomingSoundProfile.SoundProfileState = SoundProfileState.Stopped;
-					this.upcomingSoundProfile = (SoundProfile)null;
-				}
-			}
-			this.stateTransition.Transition(soundProfileFromIndex, new SdLibSoundProfileStateGraph.TransitionDelegate(base.CreateSound));
-			if (this.stateTransition.Transition(soundProfileFromIndex, new SdLibSoundProfileStateGraph.TransitionDelegate(this.StartSoundCrossfadeIn)) == 0)
-			{
-				this.fadeOutDuration = (Single)fadeIn / 1000f;
-				if (this.activeSoundProfile != null)
-				{
-					ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_Stop(this.activeSoundProfile.SoundID, fadeIn);
-					this.activeSoundProfile.SoundProfileState = SoundProfileState.Stopped;
-					this.activeSoundProfile = (SoundProfile)null;
-				}
-			}
-		}
-	}
-
 	private void StartSoundCrossfadeIn(SoundProfile soundProfile)
 	{
 		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_Start(soundProfile.SoundID, 0);
@@ -230,139 +127,16 @@ public class VoicePlayer : SoundPlayer
 	{
 	}
 
-	public void PauseMusic()
-	{
-		this.stateTransition.Transition(this.activeSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.PauseSound));
-		this.stateTransition.Transition(this.upcomingSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.PauseSound));
-	}
-
-	public void ResumeMusic()
-	{
-		this.stateTransition.Transition(this.activeSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.ResumeSound));
-		this.stateTransition.Transition(this.upcomingSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.ResumeSound));
-	}
-
-	public Int32 GetActiveSoundID()
-	{
-		if (this.activeSoundProfile != null)
-		{
-			return this.activeSoundProfile.SoundID;
-		}
-		return -1;
-	}
-
-	public void StopMusic()
-	{
-		if (this.stateTransition.Transition(this.activeSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.StopSound)) == 0)
-		{
-			this.activeSoundProfile = (SoundProfile)null;
-		}
-		if (this.stateTransition.Transition(this.upcomingSoundProfile, new SdLibSoundProfileStateGraph.TransitionDelegate(base.StopSound)) == 0)
-		{
-			this.upcomingSoundProfile = (SoundProfile)null;
-		}
-	}
-
-	public void StopMusic(Int32 fadeOut)
-	{
-		if (this.activeSoundProfile != null)
-		{
-			ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_Stop(this.activeSoundProfile.SoundID, fadeOut);
-			this.activeSoundProfile.SoundProfileState = SoundProfileState.Stopped;
-			this.activeSoundProfile = (SoundProfile)null;
-		}
-		if (this.upcomingSoundProfile != null)
-		{
-			ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_Stop(this.upcomingSoundProfile.SoundID, fadeOut);
-			this.upcomingSoundProfile.SoundProfileState = SoundProfileState.Stopped;
-			this.upcomingSoundProfile = (SoundProfile)null;
-		}
-	}
-
-	public void SetOptionVolume(Int32 volume)
-	{
-		this.optionVolume = volume / 100f;
-		this.SetMusicVolume(this.playerVolume);
-	}
-
-	public void SetMusicVolume(Single volume)
-	{
-		Single oldPlayerVolume = this.playerVolume;
-		this.playerVolume = volume;
-		if (this.activeSoundProfile != null && this.activeSoundProfile.SoundProfileState == SoundProfileState.Played)
-		{
-			this.SetMusicVolume(volume, this.activeSoundProfile);
-		}
-		if (this.upcomingSoundProfile != null && this.upcomingSoundProfile.SoundProfileState == SoundProfileState.CrossfadeIn)
-		{
-			this.SetMusicVolumeWhileFade(oldPlayerVolume, volume, this.upcomingSoundProfile);
-			ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetVolume(this.upcomingSoundProfile.SoundID, volume, (Int32)(this.fadeInTimeRemain * 1000f));
-		}
-	}
-
-	private void SetMusicVolume(Single volume, SoundProfile soundProfile)
-	{
-		soundProfile.SoundVolume = this.optionVolume * volume;
-		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetVolume(soundProfile.SoundID, soundProfile.SoundVolume, 0);
-	}
-
-	private void SetMusicVolumeWhileFade(Single oldPlayerVolume, Single newPlayerVolume, SoundProfile soundProfile)
-	{
-		Single num = ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_GetVolume(soundProfile.SoundID);
-		Single volume = 0f;
-		if (oldPlayerVolume != 0f)
-		{
-			Single num2 = num / oldPlayerVolume;
-			Single num3 = newPlayerVolume - oldPlayerVolume;
-			volume = num + num3 * num2;
-		}
-		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetVolume(soundProfile.SoundID, volume, 0);
-	}
-
-	public void SetMusicPanning(Single panning)
-	{
-		this.playerPanning = panning;
-		if (this.activeSoundProfile != null)
-		{
-			this.SetMusicPanning(panning, this.activeSoundProfile);
-		}
-	}
-
 	private void SetMusicPanning(Single panning, SoundProfile soundProfile)
 	{
 		soundProfile.Panning = panning;
 		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetPanning(soundProfile.SoundID, soundProfile.Panning, 0);
 	}
 
-	public void SetMusicPitch(Single pitch)
-	{
-		this.playerPitch = pitch;
-		if (this.activeSoundProfile != null)
-		{
-			this.SetMusicPitch(pitch, this.activeSoundProfile);
-		}
-	}
-
 	private void SetMusicPitch(Single pitch, SoundProfile soundProfile)
 	{
 		soundProfile.Pitch = pitch;
 		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_SetPitch(soundProfile.SoundID, soundProfile.Pitch, 0);
-	}
-
-	public void SeekActiveSound(Int32 offsetTimeMSec)
-	{
-		if (this.activeSoundProfile == null)
-		{
-			SoundLib.Log("(activeSoundProfile == null");
-			return;
-		}
-		ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_Stop(this.activeSoundProfile.SoundID, 0);
-		this.activeSoundProfile.SoundID = ISdLibAPIProxy.Instance.SdSoundSystem_CreateSound(this.activeSoundProfile.BankID);
-		Int32 num = ISdLibAPIProxy.Instance.SdSoundSystem_SoundCtrl_Start(this.activeSoundProfile.SoundID, offsetTimeMSec);
-		if (num != 0)
-		{
-			SoundLib.LogError("startSoundErrno errors with value: " + num);
-		}
 	}
 
 	public override void Update()
