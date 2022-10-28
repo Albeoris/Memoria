2ab8d9cf6e2e35cb5fb30aae4752223e7d226268
Martin Barker <martin-barker@hotmail.co.uk>
2022-10-28 19:20:37 +0100

    removed comment that was used for refference

--- Assembly-CSharp/Memoria/Configuration/Access/Audio.cs
+++ Assembly-CSharp/Memoria/Configuration/Access/Audio.cs
@@ -49,19 +49,5 @@ namespace Memoria
                 }
             }
         }
-
-        /*
-            public static String[] FolderNames => Instance._mod.FolderNames;
-            public static String[] AllFolderNames
-            {
-                get
-                {
-                    String[] res = new String[FolderNames.Length + 1];
-                    FolderNames.CopyTo(res, 0);
-                    res[FolderNames.Length] = "";
-                    return res;
-                }
-            }
-         */
     }
 }
