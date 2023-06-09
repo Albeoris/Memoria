a267506f28ba316e0518bfc5313c497cd1a875af
DV666 <RobinCharpentier@hotmail.com>
2023-06-10 00:52:11 +0200

    Revert btl_cmd (useless, can do it on Memoria.Scripts !)

--- Assembly-CSharp/Global/btl_cmd.cs
+++ Assembly-CSharp/Global/btl_cmd.cs
@@ -1428,11 +1428,6 @@ public class btl_cmd
             if (caster.IsPlayer && FF9StateSystem.Settings.IsATBFull)
                 caster.CurrentAtb = (Int16)(caster.MaximumAtb - 1);
 
-            if (Configuration.Mod.TranceSeek && btl_util.GetCommandMainActionIndex(cmd) == (BattleAbilityId)1005) // TRANCE SEEK - Quick Attack
-            {
-                caster.CurrentAtb = (short)(caster.MaximumAtb / 2);
-            }
-            btl_para.CheckPointData(cmd.regist);
         }
 
         btlsys.cur_cmd_list.Remove(cmd);
