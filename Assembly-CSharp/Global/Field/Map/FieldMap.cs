36f0c29bf1314da3c10c8570f6acf3353ade40b6
snouz <snooz-x@hotmail.com>
2024-05-25 04:06:00 +0200

    f2200 no cam smooth

--- Assembly-CSharp/Global/Field/Map/FieldMap.cs
+++ Assembly-CSharp/Global/Field/Map/FieldMap.cs
@@ -2420,6 +2420,7 @@ public class FieldMap : HonoBehavior
         1655, // scrolling root
         1754, // Fast scroll on Iifa platform buggy
         //1953, // Scene with Quan flashback
+        2200, // Desert palace cells
         3000, 3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009, 3010, 3011, 3012, // ending glitches
     };
 }
