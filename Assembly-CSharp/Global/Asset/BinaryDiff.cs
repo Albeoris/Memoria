669c984e791f14c704076afe11f50a46107fc6ea
SamsamTS <samsamts@msn.com>
2024-06-16 11:03:06 +0200

    Removed unsafe from BinaryDiff

--- Assembly-CSharp/Global/Asset/BinaryDiff.cs
+++ Assembly-CSharp/Global/Asset/BinaryDiff.cs
@@ -115,72 +115,71 @@ public class BinaryDiff
     /// <param name="fileA">The original file</param>
     /// <param name="fileB">The modified file</param>
     /// <param name="fileB">Minimum padding between sections</param>
-    public unsafe BinaryDiff(Byte[] fileA, Byte[] fileB, Int32 padding = 4)
+    public BinaryDiff(Byte[] fileA, Byte[] fileB, Int32 padding = 4)
     {
         Padding = Math.Max(1, padding);
         TotalDiff = 0;
         Sections = new List<Section>();
         Int32 delta = 0;
 
-        fixed (Byte* a = fileA, b = fileB)
+        for (Int32 i = 0; i < fileA.Length; i++)
         {
-            for (Int32 i = 0; i < fileA.Length; i++)
+            if (i + delta >= fileB.Length)
             {
-                if (i + delta >= fileB.Length)
-                {
-                    // Reached the end of fileB prematurally
-                    Section section = new Section
-                    {
-                        start = i,
-                        end = fileA.Length,
-                        diff = i - fileA.Length,
-                        data = new Byte[0]
-                    };
-                    Sections.Add(section);
-                    TotalDiff += section.diff;
-                    return;
-                }
-                if (fileA[i] != fileB[i + delta])
-                {
-                    // Diff found
-                    Section section = new Section();
-                    FindSectionEnd(a, i, fileA.Length, out Int32 endA, b, i + delta, fileB.Length, out Int32 endB);
-                    section.start = i;
-                    section.end = endA;
-                    Int32 l = endB - (i + delta);
-                    if (l > 0)
-                    {
-                        // Data added or replaced
-                        section.data = new Byte[l];
-                        Array.Copy(fileB, i + delta, section.data, 0, l);
-                    }
-                    else
-                    {
-                        // Data removed
-                        section.data = new Byte[0];
-                    }
-                    section.diff = section.data.Length - (section.end - section.start);
-                    TotalDiff += section.diff;
-                    Sections.Add(section);
-                    i = endA - 1;
-                    delta = endB - i - 1;
-                }
-            }
-            Int32 len = fileB.Length - (fileA.Length + delta);
-            if (len > 0)
-            {
-                // Data added at the end
+                // Reached the end of fileB prematurally
                 Section section = new Section
                 {
-                    start = fileA.Length,
-                    end = fileA.Length + len,
-                    diff = len,
-                    data = new Byte[len]
+                    start = i,
+                    end = fileA.Length,
+                    diff = i - fileA.Length,
+                    data = new Byte[0]
                 };
-                Array.Copy(fileB, fileA.Length + delta, section.data, 0, len);
                 Sections.Add(section);
                 TotalDiff += section.diff;
+                return;
             }
+            if (fileA[i] != fileB[i + delta])
+            {
+                // Diff found
+                FindSectionEnd(fileA, i, out Int32 endA, fileB, i + delta, out Int32 endB);
+                Section section = new Section
+                {
+                    start = i,
+                    end = endA
+                };
+                Int32 l = endB - (i + delta);
+                if (l > 0)
+                {
+                    // Data added or replaced
+                    section.data = new Byte[l];
+                    Array.Copy(fileB, i + delta, section.data, 0, l);
+                }
+                else
+                {
+                    // Data removed
+                    section.data = [];
+                }
+                section.diff = section.data.Length - (section.end - section.start);
+                TotalDiff += section.diff;
+                Sections.Add(section);
+                i = endA - 1;
+                delta = endB - i - 1;
+            }
+        }
+        Int32 len = fileB.Length - (fileA.Length + delta);
+        if (len > 0)
+        {
+            // Data added at the end
+            Section section = new Section
+            {
+                start = fileA.Length,
+                end = fileA.Length + len,
+                diff = len,
+                data = new Byte[len]
+            };
+            Array.Copy(fileB, fileA.Length + delta, section.data, 0, len);
+            Sections.Add(section);
+            TotalDiff += section.diff;
         }
     }
 
@@ -200,11 +199,10 @@ public class BinaryDiff
             TotalDiff = diff.TotalDiff;
             return true;
         }
+
         List<Section> newSections = new List<Section>();
         foreach (Section section in Sections)
-        {
             newSections.Add(section.Copy());
-        }
         Int32 newTotal = TotalDiff;
         foreach (Section sectionA in diff.Sections)
         {
@@ -287,22 +285,20 @@ public class BinaryDiff
     }
 
     #region private
-    private unsafe void FindSectionEnd(Byte* a, Int32 startA, Int32 lenA, out Int32 endA, Byte* b, Int32 startB, Int32 lenB, out Int32 endB)
+    private void FindSectionEnd(Byte[] fileA, Int32 startA, out Int32 endA, Byte[] fileB, Int32 startB, out Int32 endB)
     {
-        Int32 maxA = lenA - startA - Padding;
-        Int32 maxB = lenB - startB - Padding;
-        endA = lenA;
-        endB = lenB;
-        Int32 maxPos = (lenA - startA) + (lenB - startB);
-        a += startA;
-        b += startB;
+        endA = fileA.Length;
+        endB = fileB.Length;
+        Int32 maxA = fileA.Length - startA - Padding;
+        Int32 maxB = fileB.Length - startB - Padding;
+        Int32 maxPos = (endA - startA) + (endB - startB);
         for (Int32 i = 0; i < maxA; i++)
         {
             if (i >= maxPos) break;
             for (Int32 j = 0; j < maxB; j++)
             {
                 if (i + j >= maxPos) break;
-                if (Equals(a + i, b + j, Padding))
+                if (Equals(fileA, startA + i, fileB, startB + j, Padding))
                 {
                     endA = startA + i;
                     endB = startB + j;
@@ -311,10 +307,10 @@ public class BinaryDiff
             }
         }
     }
-    private unsafe bool Equals(Byte* a, Byte* b, Int32 len)
+    private bool Equals(Byte[] a, Int32 startA, Byte[] b, Int32 startB, Int32 len)
     {
         for (Int32 i = 0; i < len; i++)
-            if (a[i] != b[i]) return false;
+            if (a[startA + i] != b[startB + i]) return false;
 
         return true;
     }
