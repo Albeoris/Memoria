416291326f1c2ce6b69a29354019d0edd53d2b57
DV666 <robincharpentier@hotmail.com>
2026-08-08 14:58:20 +0200

    Some comments on btl_scrp

--- Assembly-CSharp/Global/btl_scrp.cs
+++ Assembly-CSharp/Global/btl_scrp.cs
@@ -197,109 +197,109 @@ public class btl_scrp
         Int32 result = 16777215;
         switch (id)
         {
-            case 35u:
+            case 35u: // [MAX_HP]
                 result = (Int32)btl.max.hp;
                 break;
-            case 36u:
+            case 36u: // [HP]
                 result = (Int32)btl.cur.hp;
                 break;
-            case 37u:
+            case 37u: // [MAX_MP]
                 result = (Int32)btl.max.mp;
                 break;
-            case 38u:
+            case 38u: // [MP]
                 result = (Int32)btl.cur.mp;
                 break;
-            case 39u:
+            case 39u: // [MAX_ATB]
                 result = (Int32)btl.max.at;
                 break;
-            case 40u:
+            case 40u: // [ATB]
                 result = (Int32)btl.cur.at;
                 break;
-            case 41u:
+            case 41u: // [LEVEL]
                 if (btl.bi.player != 0)
                     result = btl_util.getPlayerPtr(btl).level;
                 else
                     result = btl_util.getEnemyTypePtr(btl).level;
                 break;
-            case 42u:
+            case 42u: // [STATUS_IMMUNE_B]
                 result = (Int32)(((UInt64)btl.stat.invalid >> 24) & 0xFFFFFFu);
                 break;
-            case 43u:
+            case 43u: // [STATUS_IMMUNE_A]
                 result = (Int32)((UInt64)btl.stat.invalid & 0xFFFFFFu);
                 break;
-            case 44u:
+            case 44u: // [STATUS_AUTO_B]
                 result = (Int32)(((UInt64)btl.stat.permanent >> 24) & 0xFFFFFFu);
                 break;
-            case 45u:
+            case 45u: // [STATUS_AUTO_A]
                 result = (Int32)((UInt64)btl.stat.permanent & 0xFFFFFFu);
                 break;
-            case 46u:
+            case 46u: // [STATUS_CURRENT_B]
                 result = (Int32)(((UInt64)btl.stat.cur >> 24) & 0xFFFFFFu);
                 break;
-            case 47u:
+            case 47u: // [STATUS_CURRENT_A]
                 result = (Int32)((UInt64)btl.stat.cur & 0xFFFFFFu);
                 break;
-            case 48u:
+            case 48u: // [ELEMENT_IMMUNE]
                 result = btl.def_attr.invalid;
                 break;
-            case 49u:
+            case 49u: // [ELEMENT_ABSORB]
                 result = btl.def_attr.absorb;
                 break;
-            case 50u:
+            case 50u: // [ELEMENT_HALF]
                 result = btl.def_attr.half;
                 break;
-            case 51u:
+            case 51u: // [ELEMENT_WEAK]
                 result = btl.def_attr.weak;
                 break;
-            case 52u:
+            case 52u: // [TARGETABLE]
                 result = btl.bi.target;
                 break;
-            case 53u:
+            case 53u: // [DISAPPEAR]
                 result = btl.bi.disappear;
                 break;
-            case 57u:
+            case 57u: // [MODEL]
                 result = btl.dms_geo_id;
                 break;
-            case 58u:
+            case 58u: // [MODEL_VISIBLE]
                 result = btl.mesh_current;
                 break;
-            case 64u:
+            case 64u: // [ROW]
                 result = btl.bi.row;
                 break;
-            case 65u:
+            case 65u: // [COLUMN]
                 result = btl.bi.line_no;
                 break;
-            case 66u:
+            case 66u: // [MODEL_TYPE]
                 result = (Int32)btl_util.getPlayerPtr(btl).info.serial_no;
-                break;
-            case 67u:
+               break;
+            case 67u: // [CATEGORY]
                 result = btl_util.getPlayerPtr(btl).category;
                 break;
-            case 68u:
+            case 68u: // [CLASS]
                 result = btl_util.getEnemyTypePtr(btl).category;
                 break;
-            case 69u:
+            case 69u: // [STAND_ANIMATION]
                 result = btl.bi.def_idle;
                 break;
-            case 70u:
+            case 70u: // [ENEMY_ID]
                 result = btl.bi.slot_no;
                 break;
-            case 72u:
+            case 72u: // [STRENGTH]
                 result = btl.elem.str;
                 break;
-            case 73u:
+            case 73u: // [MAGIC]
                 result = btl.elem.mgc;
                 break;
-            case 74u:
+            case 74u: // [DEFENCE]
                 result = btl.defence.PhysicalDefence;
                 break;
-            case 75u:
+            case 75u: // [EVADE]
                 result = btl.defence.PhysicalEvade;
                 break;
-            case 76u:
+            case 76u: // [MAGICAL_DEFENCE]
                 result = btl.defence.MagicalDefence;
                 break;
-            case 77u:
+            case 77u: // [MAGICAL_EVADE]
                 result = btl.defence.MagicalEvade;
                 break;
             case 100u: // access/modify an enemy's item to steal with SV_FunctionEnemy[100] and followings
