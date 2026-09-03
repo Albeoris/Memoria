3826e762052f80479f036941a60269f455ed154f
faospark <faoronkenlee@gmail.com>
2026-09-03 19:00:41 +0800

    Removed Line 81

--- Memoria.Launcher/MainWindow.cs
+++ Memoria.Launcher/MainWindow.cs
@@ -78,7 +78,6 @@ namespace Memoria.Launcher
                 _modFileSystem.DeleteTemporaryDirectory();
             }
             catch { }
-            UpdateLauncherTheme();
             HotfixForMoguriMod();
             Lang.Res["Settings.LauncherWindowTitle"] += " | v" + MemoriaAssemblyCompileDate.ToString("yyyy.MM.dd");
             Lang.Res["Settings.MemoriaEngine"] += " v" + MemoriaAssemblyCompileDate.ToString("yyyy.MM.dd");
@@ -150,7 +149,7 @@ namespace Memoria.Launcher
                     UpdateModSettings();
                     UpdateLauncherTheme();
                 }
-                else if(date < new DateTime(2025, 07, 13))
+                else if (date < new DateTime(2025, 07, 13))
                 {
                     // Make sure to use the new back-end
                     IniFile.MemoriaIni.SetSetting("Audio", "Backend", "1");
